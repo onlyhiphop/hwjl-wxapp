64a5b473f1a13a6e9ebc140034cc97adb4577131
onlyhiphop <760003259@qq.com>
2020-09-07 22:14:41 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    active: 'index',
+    imgUrls: [
+      "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2512412073,484693686&fm=27&gp=0.jpg",
+      "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=231620273,2622968107&fm=27&gp=0.jpg",
+      "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=281531042,273318123&fm=27&gp=0.jpg",
+      "http://img4.imgtn.bdimg.com/it/u=2731345960,2613387946&fm=26&gp=0.jpg"
+    ],
+    hotGoodsList: [
+      {}
+    ]
+  },
+  //事件处理函数
+  onChange() {
+    
+  },
+  onLoad: function () {
+    
+  },
+  onReady: function(){
+
+  }
+})
