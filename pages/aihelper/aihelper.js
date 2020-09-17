84cf205ba43b67041198fd304f918d7c376df7dc
onlyhiphop <760003259@qq.com>
2020-09-17 22:59:23 +0800

    测试自定义组件

--- pages/aihelper/aihelper.js
+++ pages/aihelper/aihelper.js
@@ -4,20 +4,30 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    textPro: {
+      type: String,
+      value: 'dafault value',
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    person: {
+      age: 18,
+      name: 'xaioming'
+    }
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    showToast(){
+      wx.showToast({
+        title: '成！' + this.data.person.age,
+      })
+    }
   }
 })
